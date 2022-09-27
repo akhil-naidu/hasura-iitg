362a1bfe3cc848702bbf02e26f95f2469608b732
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2022-09-27 09:28:35 +0530

    chore: configuring chakra ui

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import { Button, Heading } from '@chakra-ui/react';
 import React from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Heading as='h1'>Checking Chakra UI</Heading>
+      <Button>Click Me</Button>
+    </div>
+  );
 };
 
 export default Home;
