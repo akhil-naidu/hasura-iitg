a27c58361aa8f474e958484beed93c5668ed55ac
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2022-09-27 09:50:19 +0530

    chore: control over light and dark mode colors

--- components/FrontPage.js
+++ components/FrontPage.js
@@ -1,12 +1,20 @@
-import { Button, Heading, useColorMode } from '@chakra-ui/react';
+import {
+  Button,
+  Heading,
+  useColorMode,
+  useColorModeValue,
+} from '@chakra-ui/react';
 import React from 'react';
 
 const FrontPage = () => {
   const { toggleColorMode, colorMode } = useColorMode();
+  const color = useColorModeValue('red.500', 'red.200');
 
   return (
     <div>
-      <Heading as='h1'>Checking Chakra UI</Heading>
+      <Heading as='h1' color={color}>
+        Checking Chakra UI
+      </Heading>
       <Button onClick={toggleColorMode}>
         Toggle {colorMode === 'light' ? `dark` : 'light'}
       </Button>
