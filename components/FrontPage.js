754c97fa4201e19106b4472d4231903e810f7561
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2022-09-27 09:48:03 +0530

    chore: toggle theme

--- components/FrontPage.js
+++ components/FrontPage.js
@@ -1,11 +1,15 @@
-import { Button, Heading } from '@chakra-ui/react';
+import { Button, Heading, useColorMode } from '@chakra-ui/react';
 import React from 'react';
 
 const FrontPage = () => {
+  const { toggleColorMode, colorMode } = useColorMode();
+
   return (
     <div>
       <Heading as='h1'>Checking Chakra UI</Heading>
-      <Button>Click Me</Button>
+      <Button onClick={toggleColorMode}>
+        Toggle {colorMode === 'light' ? `dark` : 'light'}
+      </Button>
     </div>
   );
 };
