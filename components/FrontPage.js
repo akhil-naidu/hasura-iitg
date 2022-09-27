7ed228c3da8eadce6ee484bd5abc6644a7ee5ad0
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2022-09-27 09:33:08 +0530

    init: tsconfig.json

---
+++ components/FrontPage.js
@@ -0,0 +1,13 @@
+import { Button, Heading } from '@chakra-ui/react';
+import React from 'react';
+
+const FrontPage = () => {
+  return (
+    <div>
+      <Heading as='h1'>Checking Chakra UI</Heading>
+      <Button>Click Me</Button>
+    </div>
+  );
+};
+
+export default FrontPage;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,8 @@
-import { Button, Heading } from '@chakra-ui/react';
+import FrontPage from '@/components/FrontPage';
 import React from 'react';
 
 const Home = () => {
-  return (
-    <div>
-      <Heading as='h1'>Checking Chakra UI</Heading>
-      <Button>Click Me</Button>
-    </div>
-  );
+  return <FrontPage />;
 };
 
 export default Home;
